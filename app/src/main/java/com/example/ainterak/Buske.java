26ccc43f1dbc5189cea6b7c7f630015f3ff89735
Christian Bilevits <chrillebile@gmail.com>
2019-04-17 15:16:16 +0200

    PrimaryKey now autogenerates

--- app/src/main/java/com/example/ainterak/Buske.java
+++ app/src/main/java/com/example/ainterak/Buske.java
@@ -11,7 +11,7 @@ import android.arch.persistence.room.PrimaryKey;
 @Entity(tableName = "buske")
 public class Buske {
 
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     public int id;
 
     @ColumnInfo(name = "name")
