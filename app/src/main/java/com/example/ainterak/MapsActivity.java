00213c6123c9ca6ba564e5b3292411b6d81c3dc2
PontusOlssonLTH <dat15pol@student.lu.se>
2019-05-06 18:20:40 +0200

    Clean up

--- app/src/main/java/com/example/ainterak/MapsActivity.java
+++ app/src/main/java/com/example/ainterak/MapsActivity.java
@@ -2,7 +2,6 @@ package com.example.ainterak;
 
 import android.content.Intent;
 import android.location.Location;
-import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
