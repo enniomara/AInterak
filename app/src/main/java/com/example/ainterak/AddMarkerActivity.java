f1055eaef34cf2fab268f0799a0d4a20049e5dfb
PontusOlssonLTH <dat15pol@student.lu.se>
2019-05-07 16:03:42 +0200

    Forgot an import

--- app/src/main/java/com/example/ainterak/AddMarkerActivity.java
+++ app/src/main/java/com/example/ainterak/AddMarkerActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
