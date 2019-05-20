8b7ebc0f8bede0d43f101b998d4a871c1fb2e511
Christian Bilevits <chrillebile@gmail.com>
2019-05-20 10:41:04 +0200

    Make buske parcelable

--- app/src/main/java/com/example/ainterak/Buske.java
+++ app/src/main/java/com/example/ainterak/Buske.java
@@ -3,6 +3,8 @@ package com.example.ainterak;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import java.util.Date;
 
@@ -11,7 +13,7 @@ import java.util.Date;
  * @since 2019-04-17
  */
 @Entity(tableName = "buske")
-public class Buske {
+public class Buske implements Parcelable {
 
     @PrimaryKey(autoGenerate = true)
     public int id;
@@ -30,4 +32,43 @@ public class Buske {
 
     @ColumnInfo(name = "description")
     public String description;
+
+    public Buske() {
+    }
+
+    public Buske(Parcel in) {
+        this.id = in.readInt();
+        this.date = (Date) in.readSerializable();
+        this.name = in.readString();
+        this.longitude = in.readDouble();
+        this.latitude = in.readDouble();
+        this.description = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeSerializable(date);
+        parcel.writeString(name);
+        parcel.writeDouble(longitude);
+        parcel.writeDouble(latitude);
+        parcel.writeString(description);
+    }
+
+    public static final Parcelable.Creator<Buske> CREATOR = new Parcelable.Creator<Buske>() {
+        public Buske createFromParcel(Parcel in) {
+            return new Buske(in);
+        }
+
+        @Override
+        public Buske[] newArray(int i) {
+            return new Buske[i];
+        }
+    };
 }
+
