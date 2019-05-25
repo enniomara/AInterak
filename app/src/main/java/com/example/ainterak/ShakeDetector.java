1e242827839df9be41cadb2e8c491a8c4086838e
Christian Bilevits <chrillebile@gmail.com>
2019-05-25 15:03:52 +0200

    ShakeDetector now also creates the sensor; Add onResume and onPause

--- app/src/main/java/com/example/ainterak/ShakeDetector.java
+++ app/src/main/java/com/example/ainterak/ShakeDetector.java
@@ -1,9 +1,11 @@
 package com.example.ainterak;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * @author chrillebile, Christian Bilevits
@@ -18,10 +20,17 @@ public class ShakeDetector implements SensorEventListener {
     private static final int SHAKE_COUNT_RESET_TIME = 1500;
 
     private OnShakeListener mListener;
-    private long mShakeTimeStamp;
+    private long lastShakeTimeStamp;
     private int mShakeCount;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
 
-    public void setOnShakeListener(OnShakeListener listener){
+    public ShakeDetector(FragmentActivity activity) {
+        this.mSensorManager = (SensorManager) activity.getSystemService(Context.SENSOR_SERVICE);
+        this.mAccelerometer = this.mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    public void setOnShakeListener(OnShakeListener listener) {
         this.mListener = listener;
     }
 
@@ -31,7 +40,7 @@ public class ShakeDetector implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        if (mListener != null){
+        if (mListener != null) {
             float x = sensorEvent.values[0];
             float y = sensorEvent.values[1];
             float z = sensorEvent.values[2];
@@ -43,20 +52,20 @@ public class ShakeDetector implements SensorEventListener {
             // gForce will be close to 1 when there is no movement.
             float gForce = (float) Math.sqrt(gX * gX + gY * gY + gZ * gZ);
 
-            if (gForce > SHAKE_THRESHOLD_GRAVITY){
+            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
                 final long now = System.currentTimeMillis();
 
                 // ignore shake events too close to each other
-                if (mShakeTimeStamp + SHAKE_SLOP_TIME_MS > now){
+                if (lastShakeTimeStamp + SHAKE_SLOP_TIME_MS > now) {
                     return;
                 }
 
                 // reset the shake count if shake count is bigger than 3 or times up.
-                if (mShakeCount >= 3 || mShakeTimeStamp + SHAKE_COUNT_RESET_TIME < now){
+                if (mShakeCount >= 3 || lastShakeTimeStamp + SHAKE_COUNT_RESET_TIME < now) {
                     mShakeCount = 0;
                 }
 
-                mShakeTimeStamp = now;
+                lastShakeTimeStamp = now;
                 mShakeCount++;
 
                 mListener.onShake(mShakeCount);
@@ -67,5 +76,13 @@ public class ShakeDetector implements SensorEventListener {
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    public void onResume() {
+        this.mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    public void onPause() {
+        this.mSensorManager.unregisterListener(this);
     }
 }
